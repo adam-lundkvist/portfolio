8ae344e8833ee8f2ec8634c63ceb51a5ff332129
adam-lundkvist <113598210+adam-lundkvist@users.noreply.github.com>
2022-11-13 00:00:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ openMenu.addEventListener("click", function() {
     navMenu.style.display = "block";
     console.log("clicked")
     document.getElementsByTagName("body")[0].style.overflowY = "hidden";
-});
 
-document.addEventListener("mouseup", function(e) {
-    if (!navMenu.contains(e.target)) {
-        navMenu.style.display = "none";
-        document.getElementsByTagName("body")[0].style.overflowY = "scroll";
-    }
+    
+    document.addEventListener("mouseup", function(e) {
+        if (!navMenu.contains(e.target)) {
+            navMenu.style.display = "none";
+            document.getElementsByTagName("body")[0].style.overflowY = "scroll";
+        }
+    });
 });
