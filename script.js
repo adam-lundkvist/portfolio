091efd80fc1638861571f25892dcb72b8dead917
adam-lundkvist <113598210+adam-lundkvist@users.noreply.github.com>
2022-11-12 23:53:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const navMenu = document.getElementById("primary-navigation");
+
+
+const openMenu = document.getElementById("menu-bar");
+openMenu.addEventListener("click", function() {
+    navMenu.style.display = "block";
+    console.log("clicked")
+    document.getElementsByTagName("body")[0].style.overflowY = "hidden";
+});
+
+document.addEventListener("mouseup", function(e) {
+    if (!navMenu.contains(e.target)) {
+        navMenu.style.display = "none";
+        document.getElementsByTagName("body")[0].style.overflowY = "scroll";
+    }
+});
