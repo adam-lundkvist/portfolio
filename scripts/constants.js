439c5e1eb7ef92a60ad947783aa9ac81c1660b49
adam-lundkvist <113598210+adam-lundkvist@users.noreply.github.com>
2023-08-01 00:31:48 +0200

    Add files via upload

---
+++ scripts/constants.js
@@ -0,0 +1,42 @@
+const projects = [
+    {
+        name: "BookQuery",
+        description: "This React-made web app has an implemented search function that searches through The Open Library API and returns the results. A list of books is returned. The user can later view more info about a specific book.",
+        link: "https://open-library-react.netlify.app/",
+        imageLink: "./images/bookquery.png",
+        tools: [
+            "React.js",
+            "JavaScript",
+            "Bootstrap",
+            "HTML",
+            "CSS",
+            "Netlify",
+        ]
+    },
+    {
+        name: "Skoknuten",
+        description: "In this project I built the front-end of a website with a functional shopping cart. The main focus of the project is the functionality of the shopping cart, where functions such as adding products and incrementing or decrementing quantities have been implemented.",
+        link: "https://skoknuten.netlify.app/",
+        imageLink: "./images/skoknuten.PNG",
+        tools: [
+            "JavaScript",
+            "HTML",
+            "CSS",
+            "Github",
+            "Netlify",
+        ]
+    },
+];
+
+const skills = [
+    "JavaScript",
+    "React.js",
+    "HTML",
+    "CSS",
+    "Responsive Web Design",
+    "Interaction Design",
+    "Information Architecture",
+    "Bootstrap",
+    "WordPress",
+    "Figma",
+];
---
+++ scripts/script.js
@@ -0,0 +1,81 @@
+function openNav() {
+    const navEl = document.getElementById("clicked-navigation");
+    navEl.style.display = "block";
+
+    const hamMenu = document.getElementById("hamburger-open");
+    const closeNavBtn = document.getElementById("hamburger-close");
+
+    hamMenu.style.display = "none";
+    closeNavBtn.style.display = "block";
+
+    navMenu.style.display = "block";
+}
+
+function closeNav() {
+    const hamMenu = document.getElementById("hamburger-open");
+    const closeNavBtn = document.getElementById("hamburger-close");
+    const navMenu = document.getElementById("clicked-navigation");
+    navMenu.style.display = "none";
+    hamMenu.style.display = "block";
+    closeNavBtn.style.display = "none";
+    document.getElementsByTagName("body")[0].style.overflowY = "scroll";
+}
+
+function renderSkills() {
+    const skillListEl = document.getElementById("skills");
+
+    for (skill of skills) {
+        const skillEl = 
+        `<li class="skill">${skill}</li>`
+        ;
+        skillListEl.innerHTML += skillEl;
+    }
+}
+
+function renderProjects() {
+    for (let project of projects) {
+        const projectName = project.name;
+        const projectDescription = project.description;
+        const projectLink = project.link;
+        const imageLink = project.imageLink;
+        const tools = project.tools;
+        let toolsList = "";
+
+        for (let tool of tools) {
+            toolsList += `<li class="tool">${tool}</li>`;
+        }
+
+        const template = 
+        `
+        <article class="project-container columns">
+        <figure class="project-img-figure column">
+          <a href="${projectLink}" target="_blank">
+            <img class="project-img" alt="${imageLink}" src="${imageLink}">
+          </a>
+        </figure>
+        <div class="project-info column">
+          <h3 class="project-title">${projectName}</h3>
+          <p>${projectDescription}</p>
+          <div class="btns">
+            <a class="project-btn links general-btns" href="${projectLink}" target="_blank">View Project <i
+            class="fa-solid fa-arrow-right project-arrow"></i></a>
+          </div>
+          <div class="tools-div">
+            <ul class="tools wrap">
+            ${toolsList}
+            </ul>
+          </div>
+        </div>
+      </article>
+        `
+    ;
+
+    const projectsEl = document.getElementById("projects");
+    projectsEl.innerHTML += template;
+    }
+}
+
+window.onload = () => {
+    renderProjects();
+    renderSkills();
+};
