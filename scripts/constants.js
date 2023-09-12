02cc8f40e2a3128d2b568f90d4651ee536a90134
adam-lundkvist <113598210+adam-lundkvist@users.noreply.github.com>
2023-09-12 08:15:22 +0200

    Update constants.js

--- scripts/constants.js
+++ scripts/constants.js
@@ -2,7 +2,7 @@ const projects = [
     {
         name: "BookQuery",
         description: "This React-made web app has an implemented search function that searches through The Open Library API and returns the results. A list of books is returned. The user can later view more info about a specific book.",
-        link: "https://open-library-react.netlify.app/",
+        link: "https://book-query-open-library.netlify.app/",
         imageLink: "./images/bookquery.png",
         tools: [
             "React.js",
