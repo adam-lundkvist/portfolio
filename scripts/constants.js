ba85897602a398521709b46fa9f5e79a3a325cac
Adam Lundkvist <adamlundkvist00@gmail.com>
2024-01-31 22:50:19 +0100

    Commented out Project till bug has been fixed

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,4 +1,5 @@
 const projects = [
+  /*
     {
         name: "BookQuery",
         description: "This React-made web app has an implemented search function that searches through The Open Library API and returns the results. A list of books is returned. The user can later view more info about a specific book.",
@@ -13,30 +14,26 @@ const projects = [
             "Netlify",
         ]
     },
-    {
-        name: "Skoknuten",
-        description: "In this project I built the front-end of a website with a functional shopping cart. The main focus of the project is the functionality of the shopping cart, where functions such as adding products and incrementing or decrementing quantities have been implemented.",
-        link: "https://skoknuten.netlify.app/",
-        imageLink: "./images/skoknuten.PNG",
-        tools: [
-            "JavaScript",
-            "HTML",
-            "CSS",
-            "Github",
-            "Netlify",
-        ]
-    },
+    */
+  {
+    name: "Skoknuten",
+    description:
+      "In this project I built the front-end of a website with a functional shopping cart. The main focus of the project is the functionality of the shopping cart, where functions such as adding products and incrementing or decrementing quantities have been implemented.",
+    link: "https://skoknuten.netlify.app/",
+    imageLink: "./images/skoknuten.PNG",
+    tools: ["JavaScript", "HTML", "CSS", "Github", "Netlify"],
+  },
 ];
 
 const skills = [
-    "JavaScript",
-    "React.js",
-    "HTML",
-    "CSS",
-    "Responsive Web Design",
-    "Interaction Design",
-    "Information Architecture",
-    "Bootstrap",
-    "WordPress",
-    "Figma",
+  "JavaScript",
+  "React.js",
+  "HTML",
+  "CSS",
+  "Responsive Web Design",
+  "Interaction Design",
+  "Information Architecture",
+  "Bootstrap",
+  "WordPress",
+  "Figma",
 ];
--- scripts/script.js
+++ scripts/script.js
@@ -1,50 +1,47 @@
 function openNav() {
-    const navEl = document.getElementById("clicked-navigation");
-    navEl.style.display = "block";
+  const navEl = document.getElementById("clicked-navigation");
+  navEl.style.display = "block";
 
-    const hamMenu = document.getElementById("hamburger-open");
-    const closeNavBtn = document.getElementById("hamburger-close");
+  const hamMenu = document.getElementById("hamburger-open");
+  const closeNavBtn = document.getElementById("hamburger-close");
 
-    hamMenu.style.display = "none";
-    closeNavBtn.style.display = "block";
+  hamMenu.style.display = "none";
+  closeNavBtn.style.display = "block";
 }
 
 function closeNav() {
-    const hamMenu = document.getElementById("hamburger-open");
-    const closeNavBtn = document.getElementById("hamburger-close");
-    const navMenu = document.getElementById("clicked-navigation");
-    navMenu.style.display = "none";
-    hamMenu.style.display = "block";
-    closeNavBtn.style.display = "none";
-    document.getElementsByTagName("body")[0].style.overflowY = "scroll";
+  const hamMenu = document.getElementById("hamburger-open");
+  const closeNavBtn = document.getElementById("hamburger-close");
+  const navMenu = document.getElementById("clicked-navigation");
+  navMenu.style.display = "none";
+  hamMenu.style.display = "block";
+  closeNavBtn.style.display = "none";
+  document.getElementsByTagName("body")[0].style.overflowY = "scroll";
 }
 
 function renderSkills() {
-    const skillListEl = document.getElementById("skills");
+  const skillListEl = document.getElementById("skills");
 
-    for (skill of skills) {
-        const skillEl = 
-        `<li class="skill">${skill}</li>`
-        ;
-        skillListEl.innerHTML += skillEl;
-    }
+  for (skill of skills) {
+    const skillEl = `<li class="skill">${skill}</li>`;
+    skillListEl.innerHTML += skillEl;
+  }
 }
 
 function renderProjects() {
-    for (let project of projects) {
-        const projectName = project.name;
-        const projectDescription = project.description;
-        const projectLink = project.link;
-        const imageLink = project.imageLink;
-        const tools = project.tools;
-        let toolsList = "";
+  for (let project of projects) {
+    const projectName = project.name;
+    const projectDescription = project.description;
+    const projectLink = project.link;
+    const imageLink = project.imageLink;
+    const tools = project.tools;
+    let toolsList = "";
 
-        for (let tool of tools) {
-            toolsList += `<li class="tool">${tool}</li>`;
-        }
+    for (let tool of tools) {
+      toolsList += `<li class="tool">${tool}</li>`;
+    }
 
-        const template = 
-        `
+    const template = `
         <article class="project-container columns">
         <figure class="project-img-figure column">
           <a href="${projectLink}" target="_blank">
@@ -65,15 +62,13 @@ function renderProjects() {
           </div>
         </div>
       </article>
-        `
-    ;
-
+        `;
     const projectsEl = document.getElementById("projects");
     projectsEl.innerHTML += template;
-    }
+  }
 }
 
 window.onload = () => {
-    renderProjects();
-    renderSkills();
+  renderProjects();
+  renderSkills();
 };
